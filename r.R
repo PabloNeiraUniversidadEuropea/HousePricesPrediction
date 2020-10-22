3530c99c33f4df59f84c20bdbe334b2eacd7cb7d
PabloNeiraUniversidadEuropea <pneira17@gmail.com>
2020-10-22 11:17:32 +0200

    conversion

--- r.R
+++ r.R
@@ -1,5 +1,14 @@
 ## read data #####
 
 house <- 23
+cambiohoy <- 222
 ## procdssing ####
 ## final outcome####
+
+# PYTHON Y R
+# R es mucho más rápido que Python, sobre todo cuando tiene más interacciones.
+# R tiene poca  conectividad. Python es multiparadigma, entonces en estadística mejor.
+# La parte buena es que R tiene unos gráficos espectaculares vs Python.
+# Ambos tienen una comunidad brutal de apoyo.
+# R era famoso por los paquetes estadísticos, pero Python ahora le ha superado por mucho.
+
